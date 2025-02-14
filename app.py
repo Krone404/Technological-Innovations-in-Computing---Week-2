c69584ed6589d85b6d8ef2f3e65a788fc9ac04a8
Cameron <cameron.cartwright122804@gmail.com>
2025-02-14 10:58:01 +0000

    Update app.py

--- app.py
+++ app.py
@@ -13,5 +13,5 @@ def divide(a, b):
 if __name__ == "__main__":
   print(f"2 + 3 = {add(2, 3)}")
   print(f"7 - 5 = {subtract(7, 5)}")
-  print(f"2 * 2 = {mutliply(2, 2}")
-  print(f"6 / 3 = {divide(6, 3}")
+  print(f"2 * 2 = {mutliply(2, 2)}")
+  print(f"6 / 3 = {divide(6, 3)}")
