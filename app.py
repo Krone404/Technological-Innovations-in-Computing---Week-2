5e7cd368925a5f1a93ff7f0eabaf2129079b616d
Cameron <cameron.cartwright122804@gmail.com>
2025-02-14 10:52:07 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,17 @@
 def add(a, b):
   return a + b
+
+def subtract(a, b):
+  return a - b
+
+def multiply(a, b):
+  return a * b
+
+def divide(a, b):
+  return a / b
   
 if __name__ == "__main__":
   print(f"2 + 3 = {add(2, 3)}")
+  print(f"7 - 5 = {subtract(7, 5)}")
+  print(f"2 * 2 = {mutliply(2, 2}")
+  print(f"6 / 3 = {divide(6, 3}")
